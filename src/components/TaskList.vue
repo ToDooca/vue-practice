cb1db133d1a006ac4a5ad5e834df8a9bf8819fff
Dušan Stanković <d.stankovic@vegait.rs>
2022-04-15 15:10:35 +0200

    added delete task method

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,6 +1,8 @@
 <template>
   <div :key="task.id" v-for="task in tasks">
-    <Task :task="task"/>
+    <Task
+        @delete-task="$emit('delete-task', task.id)"
+        :task="task"/>
   </div>
 </template>
 
@@ -13,8 +15,9 @@ export default {
     tasks: Array
   },
   components: {
-    Task
-  }
+    Task,
+  },
+  emits: ['delete-task'],
 }
 </script>
 
